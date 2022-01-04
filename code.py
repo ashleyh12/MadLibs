a955828592fb44cf14b78737f868b8321ba3df6c
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-01-04 15:33:02 -0500

    Update code.py

--- code.py
+++ code.py
@@ -18,3 +18,4 @@ print("\n")
 
 
 print("Today I went to the zoo. I saw a(n) " + adj1 +  " " + noun1 + " jumping up and down in its tree." + " " + "He " + pluralnoun + " " + adverb1 + " through the large tunnel that led to its " + adj1 + " " + noun1 + ". I got some peanuts and passed them through the cage to a gigantic gray " + noun1 + " towering above my head." + " " + "Feeding that animal made me hungry. I went to get a " + adj1 + " scoop of ice cream. It filled my stomach." + " " + "Afterwards I had to " + " " + verb1 + " " + adverb1 + " to catch our bus. When I got home I " + pastverb + " to my mom for a(n) " + adj1 + " day at the zoo.")
+ 
