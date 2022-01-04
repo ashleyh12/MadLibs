d8c638ed4b3eef7ea426bbded6bfad0fd93e087c
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-01-04 14:56:07 -0500

    Update code.py

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
-hi
+# Source for madLib: https://irp-cdn.multiscreensite.com/ec2b0ab8/files/uploaded/Mad%20Libs.pdf
+
+#Questions go here
+
+adj1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun: ")
+pluralnoun = input("Enter a plural noun: ")
+verb1 = input("Enter a verb: ")
+adverb1 = input("Enter an adverb: ")
+pastverb = input("Enter a past tense verb: ")
+
+#created a line break to make the output look better
+print("\n")
+
+
+
+#MadLib prints out below
+
+
+print("Today I went to the zoo. I saw a(n) " + adj1 +  " " + noun1 + " jumping up and down in its tree." + " " + "He " + pluralnoun + " " + adverb1 + " through the large tunnel that led to its " + adj1 + " " + noun1 + ". I got some peanuts and passed them through the cage to a gigantic gray " + noun1 + " towering above my head." + " " + "Feeding that animal made me hungry. I went to get a " + adj1 + " scoop of ice cream. It filled my stomach." + " " + "Afterwards I had to " + " " + verb1 + " " + adverb1 + " to catch our bus. When I got home I " + pastverb + " to my mom for a(n) " + adj1 + " day at the zoo.")
