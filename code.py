8f998cf5006503278bb8157ea77f8d677f66f478
ashley <90946757+ashleyh12@users.noreply.github.com>
2022-01-05 15:58:32 -0500

    Update code.py

--- code.py
+++ code.py
@@ -4,7 +4,6 @@
 
 adj1 = input("Enter an adjective: ")
 noun1 = input("Enter a noun: ")
-pluralnoun = input("Enter a plural noun: ")
 verb1 = input("Enter a verb: ")
 adverb1 = input("Enter an adverb: ")
 pastverb = input("Enter a past tense verb: ")
@@ -17,5 +16,4 @@ print("\n")
 #MadLib prints out below
 
 
-print("Today I went to the zoo. I saw a(n) " + adj1 +  " " + noun1 + " jumping up and down in its tree." + " " + "He " + pluralnoun + " " + adverb1 + " through the large tunnel that led to its " + adj1 + " " + noun1 + ". I got some peanuts and passed them through the cage to a gigantic gray " + noun1 + " towering above my head." + " " + "Feeding that animal made me hungry. I went to get a " + adj1 + " scoop of ice cream. It filled my stomach." + " " + "Afterwards I had to " + " " + verb1 + " " + adverb1 + " to catch our bus. When I got home I " + pastverb + " to my mom for a(n) " + adj1 + " day at the zoo.")
- 
+print(f"Today I went to the zoo. I saw a {adj1} {noun1} jumping up and down in its tree. He {pastverb}  {adverb1} through the large tunnel that led to its {adj1} {noun1}. I got some peanuts and passed them through the cage to a gigantic gray {noun1} towering above my head. Feeding that animal made me hungry. I went to get a {adj1} scoop of ice cream. It filled my stomach. Afterwards I had to {verb1} {adverb1} to catch our bus. When I got home I {pastverb} to my mom for a(n) {adj1} day at the zoo.")
